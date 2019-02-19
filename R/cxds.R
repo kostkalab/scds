28e1bf8cae740a1f1ca03ab18ed2c3b6225f5033
Dennis Kostka <kostka@pitt.edu>
2019-02-18 19:20:20 -0500

    Added co-expression based doublet scoring. Added some content to the readme.

---
+++ R/cxds.R
@@ -0,0 +1,108 @@
+#' Find doublets/multiples in UMI scRNA-seq data;
+#'
+#' @param sce single cell experiment to analyze; needs "counts" in assays slot.
+#' @param ntop number of top variance genes to consider. Default: 500
+#' @param binThresh more than this many counts to consider a gene present in a cell. Default: 0
+#' @param verb progress messages. Default: FALSE
+#' @param retRes logical. Return gene pair socres & top-scoring gene pairs? Default: FALSE.
+#' @return sce Input sce with doublet scores added to colData as "mg_dbl_score" column.
+#' @importFrom Matrix Matrix rowSums rowMeans
+#' @import  SingleCellExperiment
+#' @importFrom SummarizedExperiment assay assay<- assays assays<- assayNames rowData rowData<- colData colData<-
+#' @importFrom S4Vectors SimpleList
+#' @importFrom S4Vectors metadata 'metadata<-'
+#' @importFrom stats pbinom
+#' @export
+cxds <- function( sce, ntop=500, binThresh=0, verb=FALSE, retRes=FALSE){
+#===========================================================================================
+
+  #- 1. Binarize counts
+  if(verb) cat("\n-> binarizing counts\n")
+  B = counts(sce) > binThresh
+
+  #- 2. Select high-variance genes
+  if(verb) cat("-> selecting genes\n")
+  ps  = Matrix::rowMeans(B)
+  vrs = ps*(1-ps)
+  hvg = order(vrs,decreasing=TRUE)[1:ntop]
+  Bp  = B[hvg,]
+
+  #- score all pairs
+  #- Simple model for (01) and (10) observations per gene pair
+  if(verb) cat("-> scoring gene pairs\n")
+  ps    = Matrix::rowMeans(Bp)
+  prb   = outer(ps,1-ps)
+  prb   = prb + t(prb)
+  obs   = Bp %*% (1-t(Bp))
+  obs   = obs + t(obs)
+  #- log p-vals for the observed numbers of 01 and 10
+  pvmat = stats::pbinom(as.matrix(obs)-1,prob=prb,size=ncol(Bp),lower.tail=FALSE,log=TRUE)
+  #- scores
+  if(verb) cat("-> calcluating cell scores\n")
+  scores = Matrix::diag(t(Bp)%*%pvmat%*%Bp)
+
+  if(retRes){
+    if(verb) cat("-> prioritizing gene pairs\n")
+    res = list(scores=-scores, S=-pvmat, hvg=hvg, binThresh=binThresh)
+    #- rank gene pairs by wighted average doublet contribution
+    tmp = t(t(Bp) *(-scores))
+    tmp = tmp %*% t(Bp)
+    tmp = tmp * pvmat
+    pvmat = as.matrix(tmp)
+
+    colnames(pvmat) = 1:ncol(pvmat)
+    rownames(pvmat) = colnames(pvmat)
+    topPrs = matrix(NA,nrow=100,ncol=2)
+    for(i in 1:100){
+      pr = which(pvmat == min(pvmat) ,arr.ind=TRUE)[1:2]
+      topPrs[i,] = as.integer(colnames(pvmat)[pr])
+      pvmat = pvmat[-pr,]
+      pvmat = pvmat[,-pr]
+    }
+    res$topPairs = topPrs
+
+    #- put result in sce
+    if(verb) cat("-> done.\n\n")
+    hvg_bool                     = (1:nrow(sce)) %in% res$hvg
+    hvg_ord                      = rep(NA,nrow(sce))
+    hvg_ord[hvg_bool]            = res$hvg
+    rowData(sce)$cxds_hvg_bool   = hvg_bool
+    rowData(sce)$cxds_hvg_ordr   = hvg_ord
+    metadata(sce)$cxds_S         = res$S
+    metadata(sce)$cxds_topPairs  = res$topPairs
+    metadata(sce)$cxds_binThresh = res$binThresh
+    sce$cxds_score               = res$scores
+  } else{
+    if(verb) cat("-> done.\n\n")
+    sce$cxds_score = -scores;
+  }
+  return(sce)
+}
+
+#' Extract top-scoring gene pairs from an SingleCellExperiment where cxds has been run
+#'
+#' @param sce single cell experiment to analyze; needs "counts" in assays slot.
+#' @param n integer. The number of gene pairs to extract. Default: 100
+#' @return matrix Matrix with two colulmns, each containing gene indexes for gene pairs (rows).
+cxds_getTopPairs <- function(sce,n=100){
+#=======================================
+
+  ind = rowData(sce)$cxds_hvg_ordr[!is.na(rowData(sce)$cxds_hvg_ordr)]
+  Bp  = counts(sce)[ind,] > metadata(sce)$cxds_binThresh
+  imp = t(t(Bp) * sce$cxds_score)
+  imp = imp  %*% t(Bp)
+  imp = imp * metadata(sce)$cxds_S
+  imp = as.matrix(imp)
+  res = list(imp=imp)
+
+  colnames(imp) = 1:ncol(imp)
+  rownames(imp) = colnames(imp)
+  topPrs = matrix(NA,nrow=n,ncol=2)
+  for(i in 1:n){
+    pr = which(imp == max(imp) ,arr.ind=TRUE)[1:2]
+    topPrs[i,] = as.integer(colnames(imp)[pr])
+    imp = imp[-pr,]
+    imp = imp[,-pr]
+  }
+  res$topPairs = topPrs
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # scds
-In-silico doublet annotation for single cell RNA sequencing data
+
+**s**ingle **c**ell **d**oublet **s**coring: In-silico doublet annotation for single cell RNA sequencing data
+
+```scds``` is an ```R``` package for computational doublet annotation of single cell RNA sequencing data. It interfaces with the S4 ```SingleCellExperiment``` class [CITE], so it should easily integrate int many ```R```/Bioconductor analysis workflows. 
+
+#### Installation
+
+In the following ```sce``` is a ```SingleCellExperiment``` holding at least raw counts in an assay called ```counts```.
+
+```
+devtools::install_github('kostkalab/scds')
+```
+
+#### Quick Start
+
+```
+#- Annotate doublet using co-expression based doublet scoring:
+sce = cxds(sce)
+
+#- Annotate doublet using binary classification based doublet scoring:
+sce = bcds(sce)
+
+#- Combine both annotations into a hybrid annotation
+sce = cxds_bcds_hybrid(sce)
+
+#- Doublet scores are now available via colData:
+CD  = colData(sce)
+head(cbind(CD$cxds_score,CD$bcds_score, CD$hybrid_score))
+rm(CD)
+
+#- Visualize the top 5% annotated doublets
+plotDbl(sce, score="cxds_score",   frac=0.05)
+plotDbl(sce, score="bcds_score",   frac=0.05)
+plotDbl(sce, score="hybrid_score", frac=0.05)
+
+```
+
+
